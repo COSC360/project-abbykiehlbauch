fbd880f9a762e8acfbb11348d1f76931b5a9b7ff
abbykiehlbauch <abbykiehlbauch@gmail.com>
2023-02-25 22:54:12 -0800

    css changes, js changes

---
+++ script/profile.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function(){
+    document.getElementById("logout").addEventListener("click", function(e){
+        console.log("test");
+        window.location.assign("login.html");
+    });
+});
