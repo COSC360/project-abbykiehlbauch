bb7438a79141c9cb1008850136a9c6c8727794cd
abbykiehlbauch <abbykiehlbauch@gmail.com>
2023-02-26 16:24:37 -0800

    client side validation

---
+++ script/add-item.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function(){
+    document.getElementById("newItem").addEventListener("submit", function(e){
+        var fields = document.getElementsByTagName("input");
+        for(let i = 0; i < fields.length-2; i++){
+            if(fields[i].value == "")
+            {
+                e.preventDefault();
+            }
+        }
+    });
+});
