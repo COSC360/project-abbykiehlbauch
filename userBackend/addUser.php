9cdc27e6fe72ab7e7358cee97a4e19964d2ccc2e
abbykiehlbauch <abbykiehlbauch@gmail.com>
2023-03-25 16:41:02 -0700

    adding server security for add user

--- userBackend/addUser.php
+++ userBackend/addUser.php
@@ -8,18 +8,20 @@ include "../dbConnection.php";
         die("Connection failed: " . $conn->connect_error);
     }    
     // prepare and bind
-    if($_GET['q'] == 'user')
-        $stmt = $conn->prepare("INSERT INTO users (emailAddress, fname, lname, username, `password`) VALUES (?, ?, ?, ?, ?)");
-    else
-        $stmt = $conn->prepare("INSERT INTO adminuser (email, fname, lname, username, `password`) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssss",$_POST['email'], $_POST['fname'],$_POST['lname'],$_POST['username'],$_POST['password']);
-    $stmt->execute();
-    $_SESSION['username'] = $_POST['username'];
-    if($_GET['q'] == 'user')
-        header('location: ../profile.php'); //redirect to homepage
-    else
-        header('location: ../admin-profile.php');
+    if(isset($_POST['email']) && isset($_POST['fname']) && isset($_POST['lname'])&& isset($_POST['username'])&& isset($_POST['password'])){
+        if($_GET['q'] == 'user')
+            $stmt = $conn->prepare("INSERT INTO users (emailAddress, fname, lname, username, `password`) VALUES (?, ?, ?, ?, ?)");
+        else
+            $stmt = $conn->prepare("INSERT INTO adminuser (email, fname, lname, username, `password`) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss",$_POST['email'], $_POST['fname'],$_POST['lname'],$_POST['username'],$_POST['password']);
+        $stmt->execute();
+        $_SESSION['username'] = $_POST['username'];
+        if($_GET['q'] == 'user')
+            header('location: ../profile.php'); //redirect to homepage
+        else
+            header('location: ../admin-profile.php');
 
-    $stmt->close();
-    $conn->close();
+        $stmt->close();
+        $conn->close();
+    }
 ?>
