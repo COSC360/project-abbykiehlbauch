a0f830b66a623ea84d3594ff59a24fabcb2f34e4
abbykiehlbauch <abbykiehlbauch@gmail.com>
2023-04-05 00:20:18 -0700

    adjustments to hotItems page

--- hotItems.php
+++ hotItems.php
@@ -13,19 +13,6 @@
    <script type="text/javascript" src="script/search.js"></script>
    <script src="https://ajax.aspnetCDN.com/ajax/jQuery/jQuery-3.3.1.min.js"></script>
    <script>
-        //call php file to generate results
-        function results(){
-            var store = document.getElementById("dropdown").value;
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function(){
-                if (this.readyState == 4 && this.status == 200){
-                    document.getElementById("results").innerHTML = this.responseText;
-                }
-            };
-            xmlhttp.open("GET", "userBackend/getHotItems.php?s="+store);
-            xmlhttp.send();
-            return false;
-        }
         //call PHP file to generate more-info section
         function addListener(prodId){
             var moreInfo = document.getElementById("more-info");
@@ -52,26 +39,34 @@
         include "adminHeader.php";
     }
     ?>
-<div id = "search-bar">
-    <p>
-        <form id = "search-bar" >
-            <select id = "dropdown" onclick = "return results()">
-            <option> </option>
-            <?php
-                $sql = "SELECT store FROM products GROUP BY store";
-                $result = mysqli_query($conn,$sql);
-                while($row = mysqli_fetch_array($result)){
-                    echo "<option>".$row['store']."</option>";
-                }
-            ?>
-            </select>
-        </form>
-    </p>
-</div>
+
 <div id="main">
     <h1>Hot Items</h1>
     <div id="results"></div>
-    <?php include 'getHotItems.php' ?>
+    <?php
+    $sql = "SELECT productId, COUNT(*) as count FROM prices WHERE priceDate >= DATE_SUB(NOW(), INTERVAL 24 HOUR) GROUP BY productId HAVING count > 3";
+    $result = mysqli_query($conn, $sql);
+    while($prod = mysqli_fetch_array($result)){
+        $sql2 = "SELECT * FROM products WHERE productId = ".$prod['productId'];
+        $result2 = mysqli_query($conn,$sql2);
+        while($row = mysqli_fetch_array($result2)){
+            echo "<div id = item-entry>";
+            echo"<h3>".$row['productName']."</h3>";
+            echo"<p>
+                <ul>
+                    <li>$".$row['currPrice']."</li>
+                    <li>".$row['productBrand']."</li>
+                    <li>".$row['store']."</li>
+                </ul>
+            </p>
+            <p>".$row['description']."</p>";
+            echo "<input id = 'see-more' type = 'button' value = 'See more' onclick = 'return addListener(".$row['productId'].")'>";
+            echo "</div>";
+        };
+    };
+    mysqli_close($conn);
+
+?>
     <div id="center">
         <div id="more-info">
         <div id = "product-info"></div>
