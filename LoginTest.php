f693031acc55f10df17db63f33efea13817bc953
Abby Kiehlbauch <abbyk02@s129.ok.ubc.ca>
2023-04-05 20:04:48 -0700

    finalizing tests

--- LoginTest.php
+++ LoginTest.php
@@ -3,42 +3,33 @@ use PHPUnit\Framework\TestCase;
 
 class LoginTest extends TestCase
 {
-    public function testSuccessfulLogin()
-    {
-        // Set up the test data
-        $submit = true;
-        $user = 'johndoe';
-        $pass = '12345';
-        $u = 'user';
-
-	    $_POST['submitlogin'] = $submit;
-        $_POST['username'] = $user;
-        $_POST['password'] = $pass;
-        $_GET['q'] = $u;
-
-        // Include the code to be tested
-        include 'authenticationTest.php';
-        // Check if the user was redirected to the correct page
-        $this->assertEquals('profile.php', basename($_SERVER['REQUEST_URL']));
-    }
+    
 
     public function testInvalidLogin()
     {
-        // Set up the test data
-        $submit = true;
-	$user = 'johndoe';
-	$pass = '12345';
-	$u = 'user';
-
-	$_POST['submitlogin'] = $submit;
-        $_POST['username'] = $user;
-        $_POST['password'] = $pass;
-        $_GET['q'] = $u;
+        //Set up the test data
+        $username = 'johndoe';
+        $password = '123';
+        $u = 'user';
 
         // Include the code to be tested
         include 'authenticationTest.php';
 
         // Check if an error message was displayed
-        $this->expectOutputString('Invalid username or password.');
+	$msg ='Invalid username or password.';
+        $this->expectOutputString($msg);
+    }
+public function testSuccessfulLogin()
+    {
+        // Set up the test data
+        $username = 'bobbybones';
+        $password = '12345';
+        $u = 'user';
+
+	// Include the code to be tested
+        include 'authenticationTest.php';
+        // Check if the user was redirected to the correct page
+	$link='profile.php';
+        $this->expectOutputString($link);
     }
 }
