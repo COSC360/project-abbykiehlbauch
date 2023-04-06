ef14d9152db53ea6896668a0d12daec4218c44b2
abbykiehlbauch <abbykiehlbauch@gmail.com>
2023-04-05 18:01:43 -0700

    updating test

--- LoginTest.php
+++ LoginTest.php
@@ -6,21 +6,18 @@ class LoginTest extends TestCase
     public function testSuccessfulLogin()
     {
         // Set up the test data
-        
+        $submit = true;
+        $user = 'johndoe';
+        $pass = '12345';
+        $u = 'user';
 
-        // Include the code to be tested
-        include 'authentication.php';
-	
-	$submit = true;
-	$user = 'johndoe';
-	$pass = '12345';
-	$u = 'user';
-
-	$_POST['submitlogin'] = $submit;
+	    $_POST['submitlogin'] = $submit;
         $_POST['username'] = $user;
         $_POST['password'] = $pass;
         $_GET['q'] = $u;
 
+        // Include the code to be tested
+        include 'authenticationTest.php';
         // Check if the user was redirected to the correct page
         $this->assertEquals('profile.php', basename($_SERVER['REQUEST_URL']));
     }
@@ -39,7 +36,7 @@ class LoginTest extends TestCase
         $_GET['q'] = $u;
 
         // Include the code to be tested
-        include 'authentication.php';
+        include 'authenticationTest.php';
 
         // Check if an error message was displayed
         $this->expectOutputString('Invalid username or password.');
---
+++ authenticationTest.php
@@ -0,0 +1,47 @@
+<?php
+if (isset($_POST['submitlogin'])) {
+    
+    //connect to the database
+    include "dbConnection.php";
+    
+    $conn = new mysqli($connString, $user, $pass, $dbname);
+
+    //get the user input
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    //sanitize the input
+    $username = mysqli_real_escape_string($conn, $username);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    //query the database
+    if($_GET['q'] == 'user'){
+        $stmt = $conn->prepare("SELECT * FROM users WHERE username=? AND password=?");
+        $stmt->bind_param("ss",$username,md5($password));
+    }
+    else{
+        $stmt = $conn->prepare("SELECT * FROM adminuser WHERE username=? AND password=?");
+        $stmt->bind_param("ss",$username,$password);
+    }
+        
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if (mysqli_num_rows($result) == 1) {
+        $_SESSION['username'] = $username;
+        $_SESSION['admin'];
+        if($_GET['q'] == 'user')
+            header('location: profile.php'); //redirect to homepage
+        else{
+            $_SESSION['admin'] = "true";
+            header('location: admin-profile.php');
+        }
+        exit();
+    } else {
+        echo "Invalid username or password.";
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
